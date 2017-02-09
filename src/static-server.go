cf85b9a86d7e78250c80f4c8933890939b87dec3
Mark Wilson <mark@89allport.co.uk>
2017-02-09 01:09:25 +0000

    Refactor

--- src/static-server.go
+++ src/static-server.go
@@ -6,27 +6,101 @@ import (
     "path/filepath"
     "flag"
     "strconv"
-    "fmt"
+    "os"
+    "errors"
 )
 
-func main() {
-    portPointer   := flag.Int("p", 8080, "The port to listen on")
-    exposePointer := flag.Bool("x", false, "Expose the server outside of localhost")
+type FileHandler struct {
+    http.Handler
+}
 
-    flag.Parse()
+func (h FileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    log.Printf("Request for %s", r.RequestURI)
+    h.Handler.ServeHTTP(w, r)
+}
 
-    var bindAddress string
+type IpAddressFlag struct {
+    set bool
+    value string
+}
 
-    if *exposePointer {
-        bindAddress = "0.0.0.0:" + strconv.Itoa(*portPointer)
-    } else {
-        bindAddress = "127.0.0.1:" + strconv.Itoa(*portPointer)
+func (f *IpAddressFlag) Set(ip string) error {
+    f.value = ip
+    f.set = true
+
+    return nil
+}
+
+func (f *IpAddressFlag) String() string {
+    return f.value
+}
+
+type CommandFlagSet struct {
+    *flag.FlagSet
+    port *int
+    expose *bool
+    directory *string
+    bindAddress *IpAddressFlag
+}
+
+func (f *CommandFlagSet) Validate() error {
+    if f.bindAddress.set && *f.expose {
+        // TODO: add usage & invalid message to output
+        f.PrintDefaults()
+        return errors.New("Invalid flags")
     }
 
-    fmt.Printf("Listening on http://%s/\n", bindAddress)
+    return nil
+}
 
-    path := filepath.Dir(".")
-    http.Handle("/", http.FileServer(http.Dir(path)))
+func (f *CommandFlagSet) Parse(arguments []string) error {
+    f.FlagSet.Parse(arguments)
+
+    err := f.Validate()
+    if err != nil {
+        os.Exit(2)
+    }
+
+    return nil
+}
+
+func NewCommandFlagSet() CommandFlagSet {
+    f := CommandFlagSet{
+        FlagSet: flag.NewFlagSet(os.Args[0], flag.ExitOnError),
+    }
+
+    f.port      = f.Int("p", 8080, "The port to listen on")
+    f.expose    = f.Bool("x", false, "Shorthand for -i 0.0.0.0")
+    f.directory = f.String("d", ".", "Path of static files to host")
+
+    var bindAddress IpAddressFlag
+    f.Var(&bindAddress, "i", "Specify a bind address - can not be used in conjunction with the -x flag")
+    bindAddress.value = "127.0.0.1"
+    f.bindAddress = &bindAddress
+
+    return f
+}
+
+func main() {
+    f := NewCommandFlagSet()
+    f.Parse(os.Args[1:])
+
+    bindAddress := (*f.bindAddress).String()
+    expose      := *f.expose
+    port        := strconv.Itoa(*f.port)
+    directory   := *f.directory
+
+
+    if expose {
+        bindAddress = "0.0.0.0"
+    }
+
+    bindAddress += ":" + port
+
+    log.Printf("Listening on http://%s/\n", bindAddress)
+
+    path := filepath.Dir(directory)
+    http.Handle("/", FileHandler{Handler: http.FileServer(http.Dir(path))})
 
     log.Fatal(http.ListenAndServe(bindAddress, nil))
 }
